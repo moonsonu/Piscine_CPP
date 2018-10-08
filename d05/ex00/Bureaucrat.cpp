2fe379c63940821c23d144af754b0f9111fd8feb
moonyoning <moon@moonyoningui-MacBook-Pro.local>
2018-10-08 14:14:05 -0700

    d05

---
+++ d05/ex00/Bureaucrat.cpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ksonu <ksonu@student.42.us.org>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/10/08 13:02:16 by ksonu             #+#    #+#             */
+/*   Updated: 2018/10/08 13:02:18 by ksonu            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+Bureaucrat::Bureaucrat()
+{
+	return ;
+}
+
+Bureaucrat::~Bureaucrat()
+{
+	return ;
+}
+
+Bureaucrat::GradeTooHighException()
+{
+}
+
+void	Bureaucrat::getName()
+{
+	return (this->_name);
+}
+
+void	Bureaucrat::getGrade()
+{
+	return (this->_grade);
+}
+
+std::ostream & Bureaucrat::operator<<(std::string &o, Bureaucrat const &b)
+{
+	o << "<" << b.getName() << ">, " << "bureaucrat grade <" << b.getGrade() << ">." << std::endl;
+
+	return ();
+}
---
+++ d05/ex00/Bureaucrat.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ksonu <ksonu@student.42.us.org>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/10/08 13:02:08 by ksonu             #+#    #+#             */
+/*   Updated: 2018/10/08 13:02:09 by ksonu            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+class	Bureaucrat {
+	private:
+		std::string		_name;
+		int				_grade;
+		int				_range;
+};
+
+#endif
